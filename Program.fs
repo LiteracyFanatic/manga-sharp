423529cd1c7371b9c1587981b265cba461fddabb
Jordan Cannon <LiteracyFanatic@gmail.com>
2019-09-01 04:19:12 -0500

    Match input URL to a Provider using a regex

--- Program.fs
+++ Program.fs
@@ -1,48 +1,18 @@
-﻿open System.Text.RegularExpressions
-open FSharp.Data
-open MangaSharp
+﻿open MangaSharp
 open MangaSharp.Manga
-
-// Sample implementation for https://manganelo.com/manga/
-let extractTitle (html: HtmlDocument) =
-    let r = Regex("Read (.*) Manga Online For Free")
-    html.CssSelect("title")
-    |> Seq.head
-    |> HtmlNode.innerText
-    |> r.Match
-    |> fun m -> m.Groups.[1].Value
-
-let extractChapterUrls (html: HtmlDocument) =
-    html.CssSelect(".chapter-list .row a")
-    |> Seq.rev
-    |> Seq.map (HtmlNode.attributeValue "href")
-
-let extractChapterTitle (html: HtmlDocument) =
-    let r = Regex(".*Chapter (\d+(\.\d+)?).*")
-    html.CssSelect("title")
-    |> Seq.head
-    |> HtmlNode.innerText
-    |> r.Match
-    |> fun m -> m.Groups.[1].Value
-
-let extractImageUrls (html: HtmlDocument) =
-    html.CssSelect("#vungdoc img")
-    |> Seq.map (HtmlNode.attributeValue "src")
+open MangaSharp.Provider
 
 [<EntryPoint>]
 let main argv =
     let indexUrl = argv.[0]
-    let provider = {
-        Pattern = Regex("https://manganelo\.com/manga/.*")
-        TitleExtractor = extractTitle
-        ChapterUrlsExtractor = extractChapterUrls
-        ChapterTitleExtractor = extractChapterTitle
-        ImageExtractor = extractImageUrls
-    }
-    let mangaSource = {
+    let direction =
+        match argv.[1] with
+        | "horizontal" -> Horizontal
+        | "vertical" -> Vertical
+    let manga = {
         Url = indexUrl
-        Direction = Vertical
-        Provider = provider
+        Direction = direction
+        Provider = Provider.tryFromTable indexUrl direction |> Option.get
     }
-    downloadManga mangaSource
+    downloadManga manga
     0 // return an integer exit code
---
+++ Provider.fs
@@ -0,0 +1,57 @@
+module MangaSharp.Provider
+
+open System.Text.RegularExpressions
+open FSharp.Data
+open MangaSharp
+
+let extractTitle (cssQuery: string) (regex: Regex) = fun (html: HtmlDocument) ->
+    html.CssSelect(cssQuery)
+    |> Seq.head
+    |> HtmlNode.innerText
+    |> regex.Match
+    |> fun m -> m.Groups.[1].Value
+
+let extractChapterUrls (cssQuery: string) = fun (html: HtmlDocument) ->
+    html.CssSelect(cssQuery)
+    |> Seq.rev
+    |> Seq.map (HtmlNode.attributeValue "href")
+
+let extractChapterTitle (cssQuery: string) (regex: Regex) = fun (html: HtmlDocument) ->
+    html.CssSelect(cssQuery)
+    |> Seq.head
+    |> HtmlNode.innerText
+    |> regex.Match
+    |> fun m -> m.Groups.[1].Value
+
+let extractImageUrls (cssQuery: string) = fun (html: HtmlDocument) ->
+    html.CssSelect(cssQuery)
+    |> Seq.map (HtmlNode.attributeValue "src")
+
+let providers = [
+    {
+        Pattern = Regex("https://mangazuki\.me/manga/.*")
+        TitleExtractor = extractTitle ".post-title h1" (Regex("(.*)"))
+        ChapterUrlsExtractor = extractChapterUrls ".wp-manga-chapter a"
+        ChapterTitleExtractor = extractChapterTitle ".breadcrumb li.active" (Regex(".*Chapter (\d+(\.\d+)?).*"))
+        ImageExtractor = extractImageUrls ".wp-manga-chapter-img"
+    }
+    
+    {
+        Pattern = Regex("https://mangazuki\.me/.*")
+        TitleExtractor = extractTitle "title" (Regex("Read (.*) Manga.*"))
+        ChapterUrlsExtractor = extractChapterUrls ".wp-manga-chapter a"
+        ChapterTitleExtractor = extractChapterTitle ".breadcrumb li.active" (Regex(".*Chapter (\d+(\.\d+)?).*"))
+        ImageExtractor = extractImageUrls ".wp-manga-chapter-img"
+    }
+
+    {
+        Pattern = Regex("https://manganelo\.com/manga/.*")
+        TitleExtractor = extractTitle "title" (Regex("Read (.*) Manga Online For Free"))
+        ChapterUrlsExtractor = extractChapterUrls ".chapter-list .row a"
+        ChapterTitleExtractor = extractChapterTitle "title" (Regex(".*Chapter (\d+(\.\d+)?).*"))
+        ImageExtractor = extractImageUrls "#vungdoc img"
+    }
+]
+
+let tryFromTable (url: string) (direction: Direction) =
+    List.tryFind (fun p -> p.Pattern.IsMatch(url)) providers
