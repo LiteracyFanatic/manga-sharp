18deffb71e4a465890b69971e02c5f90bdeb30e2
Jordan Cannon <LiteracyFanatic@gmail.com>
2019-09-17 19:59:56 -0500

    Add manytoon.com provider

--- Provider.fs
+++ Provider.fs
@@ -7,19 +7,20 @@ open MangaSharp
 let extractTitle (cssQuery: string) (regex: Regex) = fun (html: HtmlDocument) ->
     html.CssSelect(cssQuery)
     |> Seq.head
-    |> HtmlNode.innerText
+    |> HtmlNode.directInnerText
     |> regex.Match
-    |> fun m -> m.Groups.[1].Value
+    |> fun m -> m.Groups.[1].Value.Trim()
 
 let extractChapterUrls (cssQuery: string) = fun (html: HtmlDocument) ->
     html.CssSelect(cssQuery)
     |> Seq.rev
     |> Seq.map (HtmlNode.attributeValue "href")
+    |> Seq.distinct
 
 let extractChapterTitle (cssQuery: string) (regex: Regex) = fun (html: HtmlDocument) ->
     html.CssSelect(cssQuery)
     |> Seq.head
-    |> HtmlNode.innerText
+    |> HtmlNode.directInnerText
     |> regex.Match
     |> fun m -> m.Groups.[1].Value
 
@@ -27,12 +28,14 @@ let extractImageUrls (cssQuery: string) = fun (html: HtmlDocument) ->
     html.CssSelect(cssQuery)
     |> Seq.map (HtmlNode.attributeValue "src")
 
+let chapterNumberRegex = Regex("Chapter (\d+(\.\d+)?)")
+
 let providers = [
     {
         Pattern = Regex("https://mangazuki\.me/manga/.*")
         TitleExtractor = extractTitle ".post-title h1" (Regex("(.*)"))
         ChapterUrlsExtractor = extractChapterUrls ".wp-manga-chapter a"
-        ChapterTitleExtractor = extractChapterTitle ".breadcrumb li.active" (Regex(".*Chapter (\d+(\.\d+)?).*"))
+        ChapterTitleExtractor = extractChapterTitle ".breadcrumb li.active" chapterNumberRegex
         ImageExtractor = extractImageUrls ".wp-manga-chapter-img"
     }
     
@@ -40,7 +43,7 @@ let providers = [
         Pattern = Regex("https://mangazuki\.me/.*")
         TitleExtractor = extractTitle "title" (Regex("Read (.*) Manga.*"))
         ChapterUrlsExtractor = extractChapterUrls ".wp-manga-chapter a"
-        ChapterTitleExtractor = extractChapterTitle ".breadcrumb li.active" (Regex(".*Chapter (\d+(\.\d+)?).*"))
+        ChapterTitleExtractor = extractChapterTitle ".breadcrumb li.active" chapterNumberRegex
         ImageExtractor = extractImageUrls ".wp-manga-chapter-img"
     }
 
@@ -48,8 +51,16 @@ let providers = [
         Pattern = Regex("https://manganelo\.com/manga/.*")
         TitleExtractor = extractTitle "title" (Regex("Read (.*) Manga Online For Free"))
         ChapterUrlsExtractor = extractChapterUrls ".chapter-list .row a"
-        ChapterTitleExtractor = extractChapterTitle "title" (Regex(".*Chapter (\d+(\.\d+)?).*"))
+        ChapterTitleExtractor = extractChapterTitle "title" chapterNumberRegex
         ImageExtractor = extractImageUrls "#vungdoc img"
+    }
+    
+    {
+        Pattern = Regex("https://manytoon\.com/comic/.*")
+        TitleExtractor = extractTitle ".post-title h3" (Regex("(.*)"))
+        ChapterUrlsExtractor = extractChapterUrls ".wp-manga-chapter a"
+        ChapterTitleExtractor = extractChapterTitle ".single-chapter-select option[selected]" chapterNumberRegex
+        ImageExtractor = extractImageUrls ".reading-content img"
     }
 ]
 
