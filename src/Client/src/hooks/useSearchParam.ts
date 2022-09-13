ada455acab21eb705358d3694b3e571985d18683
Jordan Cannon <LiteracyFanatic@gmail.com>
2022-09-13 14:22:33 -0500

    Allow passing null to remove search param from URL

--- src/Client/src/hooks/useSearchParam.ts
+++ src/Client/src/hooks/useSearchParam.ts
@@ -1,6 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 
-export function useSearchParam(paramName: string): [string | null, (value: string) => void];
+export function useSearchParam(paramName: string): [string | null, (value: string | null) => void];
 
 export function useSearchParam(paramName: string, defaultValue: string): [string , (value: string) => void];
 
@@ -8,12 +8,29 @@ export function useSearchParam(paramName: string, defaultValue?: string) {
     const [searchParams, setSearchParams] = useSearchParams();
     const searchParam = searchParams.get(paramName) || defaultValue || null;
 
-    function setSearchParam (value: string) {
-        setSearchParams({
-            ...Object.fromEntries(searchParams.entries()),
-            [paramName]: value
-        });
-    }
+    if (defaultValue) {
+        const setSearchParam = (value: string) => {
+            setSearchParams({
+                ...Object.fromEntries(searchParams.entries()),
+                [paramName]: value
+            });
+        };
 
-    return [searchParam, setSearchParam];
+        return [searchParam, setSearchParam];
+    } else {
+        const setSearchParam = (value: string | null) => {
+            if (value) {
+                setSearchParams({
+                    ...Object.fromEntries(searchParams.entries()),
+                    [paramName]: value
+                });
+            } else {
+                setSearchParams(
+                    Array.from(searchParams.entries()).filter(([k, v]) => k !== paramName)
+                );
+            }
+        };
+
+        return [searchParam, setSearchParam];
+    }
 }
