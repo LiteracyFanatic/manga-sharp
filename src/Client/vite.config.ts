f39c570360f878085543c42c4983f79416bc46f9
Jordan Cannon <LiteracyFanatic@gmail.com>
2023-08-31 22:14:46 -0500

    Remove PWA functionality
    It was causing hard to debug caching issues

--- src/Client/vite.config.ts
+++ src/Client/vite.config.ts
@@ -1,53 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
-        VitePWA({
-            registerType: "autoUpdate",
-            includeAssets: ["favicon.ico", "apple-touch-icon.png"],
-            manifest: {
-                name: "MangaSharp",
-                short_name: "MangaSharp",
-                description: "CLI manga downloader and reader with lightweight web interface",
-                icons: [
-                    {
-                        src: "192x192.png",
-                        sizes: "192x192",
-                        type: "image/png"
-                    },
-                    {
-                        src: "512x512.png",
-                        sizes: "512x512",
-                        type: "image/png"
-                    },
-                    {
-                        src: "mask-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                        purpose: "maskable"
-                    },
-                    {
-                        src: "mask-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                        purpose: "maskable"
-                    }
-                ]
-            },
-            workbox: {
-                globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
-                runtimeCaching: [
-                    {
-                        urlPattern: ({ url }) => url.pathname.startsWith("/pages/"),
-                        handler: "CacheFirst"
-                    }
-                ]
-            }
-        })
+        react()
     ],
     server: {
         proxy: {
